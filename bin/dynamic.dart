e8c807360df3dfdb9c367210bcb8b6360ab61243
adejui <adejuinurrochim@gmail.com>
2025-03-03 23:51:50 +0700

    Dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,11 @@
+void main() {
+  // dynamic = sebuah variabel yang bisa menampung data apapun
+  dynamic variable = 100;
+  print(variable);
+
+  variable = "Hello, World!";
+  print(variable);
+
+  variable = true;
+  print(variable);
+}
