cd3039ffe7b9a32af1518687d8c0fa106c54c906
adejui <adejuinurrochim@gmail.com>
2025-03-03 22:37:13 +0700

    Comment

---
+++ bin/comment.dart
@@ -0,0 +1,13 @@
+/// ini adalah komentar dokumentasi
+/// yang biasanya akan digenerate menjadi dart doc
+
+void main() {
+  // This is variable
+  var name = "Arda Guler";
+
+  /* 
+  ini adalah komentar
+  yang lebih dari satu baris
+  */
+  print(name);
+}
