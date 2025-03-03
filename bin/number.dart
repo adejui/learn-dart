9d924c5cdc3d05fe02f7cd2b2ac02534fdb8fcc7
adejui <adejuinurrochim@gmail.com>
2025-03-03 23:00:44 +0700

    Number

---
+++ bin/number.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number1 = 100000;
+  double number2 = 2.5;
+
+  print(number1);
+  print(number2);
+
+  num number3 = 3;
+  print(number3);
+
+  number3 = 3.5;
+  print(number3);
+}
