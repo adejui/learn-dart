c5b9dfa6dde38c1f947a0def896736b52e5349fe
adejui <adejuinurrochim@gmail.com>
2025-03-04 22:20:07 +0700

    If Else

---
+++ bin/if.dart
@@ -0,0 +1,12 @@
+void main() {
+  var nilaiAkhir = 90;
+  var nilaiAbsen = 90;
+
+  if (nilaiAkhir >= 80 && nilaiAbsen >= 80) {
+    print('Anda Mendapatkan Nilai A');
+  } else if (nilaiAkhir >= 70 && nilaiAbsen >= 70) {
+    print('Anda Mendapatkan Nilai B');
+  } else {
+    print('Ande Mendapatkan nilai dibawah A');
+  }
+}
