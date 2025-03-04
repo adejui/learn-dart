d67430e37a7b7e9f44f24c17560bd1bd48f50af2
adejui <adejuinurrochim@gmail.com>
2025-03-04 14:50:16 +0700

    Operator Perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(5 == 5);
+  print(5 != 5 );
+  print(5 < 5);
+  print(5 > 5);
+  print(5 >= 5);
+  print(5 <= 5);
+
+  print('arda' == 'arda');
+  print('arda' != 'arda');
+}
