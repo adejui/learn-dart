b0fbc8625dc4b690cf15b76b11adc0726a2e74aa
adejui <adejuinurrochim@gmail.com>
2025-03-04 14:44:11 +0700

    Operator Aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  var a = 100;
+  var b = 10;
+  var c = 5;
+  var d = 3;
+
+  var result1 = b + b;
+  var result2 = a - b;
+  var result3 = -5;
+  var result4 = c * c;
+  var result5 = b / d;
+  var result6 = b ~/ d;
+  var result7 = b % d;
+
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+  print(result5);
+  print(result6);
+  print(result7);
+}
