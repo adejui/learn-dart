bd7e066687efa51a398019791e2b248012af4eca
adejui <adejuinurrochim@gmail.com>
2025-03-06 03:21:19 +0700

    Field

--- bin/oop/class.dart
+++ bin/oop/class.dart
@@ -1,11 +1,20 @@
 class Person {
   
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
 }
 
 void main() {
 
   var person1 = Person();
-  print(person1);
+  person1.name = "Arda Guler";
+  person1.address = "Yogyakarta";
+  // person1.country = "Turki"; tidak bisa diubah karena final field
+  
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
 
   Person person2 = Person();
   print(person2);
