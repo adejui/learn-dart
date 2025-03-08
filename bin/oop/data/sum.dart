2915fefb620aeae5bb5c838992c2b14b6649e474
adejui <adejuinurrochim@gmail.com>
2025-03-09 03:07:38 +0700

    Typedef

--- bin/oop/data/sum.dart
+++ bin/oop/data/sum.dart
@@ -8,6 +8,9 @@ class Sum {
     return first + second;
   }
 }
+// Membuat nama alias untuk nama class yang sudah ada
+typedef Jumlah = Sum;
+typedef Total = Sum;
 
 void main() {
   var sum = Sum(10, 10);
