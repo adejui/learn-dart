b9b12022f80a1229dee8181432460db1a2efb957
adejui <adejuinurrochim@gmail.com>
2025-03-09 02:57:43 +0700

    Callable Class

---
+++ bin/oop/data/sum.dart
@@ -0,0 +1,15 @@
+class Sum {
+  int first;
+  int second;
+
+  Sum(this.first, this.second);
+
+  int call() {
+    return first + second;
+  }
+}
+
+void main() {
+  var sum = Sum(10, 10);
+  print(sum());
+}
